67780a33dfcc6c03309aa4ccb9045fe24b894752
Abdullah <abbudnasser10@gmail.com>
2026-02-23 22:25:28 +0300

    edit

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,11 +1,14 @@
 export default defineNuxtRouteMiddleware((to) => {
-  // نستخدم الكود الذي نجح معك في الكونسول
   if (import.meta.client) {
     const isAdmin = useCookie('is_admin')
     
-    // إذا كنت تحاول دخول صفحة الأدمن والكوكي غير موجود أو ليس true
-    if (to.path.startsWith('/admin') && isAdmin.value !== 'true') {
-      return navigateTo('/login')
+    // أضفنا فحصاً إضافياً لـ LocalStorage كخيار احتياطي فائق السرعة
+    const localAuth = localStorage.getItem('is_admin')
+
+    if (to.path.startsWith('/admin')) {
+      if (isAdmin.value !== 'true' && localAuth !== 'true') {
+        return navigateTo('/login')
+      }
     }
   }
 })
